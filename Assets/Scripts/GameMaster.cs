501b785624af924ef19fcdb5622f066e30d256d7
NASA <yutorin0116@gmail.com>
2025-10-15 22:58:25 +0900

    カードの生成２

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,7 +4,26 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
-    [SerializeField] Battler player;//Battlerというクラスの中のplayer」＝Battleというスクリプトの中で定義されたplayer?でも定義してないから、Battlerで定義されたものを使ってここでplayerを定義するよてこと？クラスの指定はなんの意味がある？
+    [SerializeField] Battler player;
+    //Battlerというクラスの中のplayer」＝Battleというスクリプトの中で定義されたplayer?でも定義してないから、Battlerで定義されたものを使ってここでplayerを定義するよてこと？クラスの指定はなんの意味がある？
+    //Inspectorでは、Player(Battler)と表示されてる。じゃあこのBattlerは買ってに決めれる属性？
+
     [SerializeField] Battler enemy;
-    
+    [SerializeField] CardGenerator cardGenerator;
+    //「カードを生成するためにはCardGeneratorを引っ張ってこないといけない」
+
+    private void Start()
+    {
+
+    }
+
+    //カードを生成して配る
+    void Setup()//初登場やんな？以下はSetupの定義？
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            cardGenerator.Spawn();
+        }
+    }
+
 }
